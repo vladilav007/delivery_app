17fe299e7017bbf4364fdd37db9348e1e33e421f
Vladyslav Vorobei <v.vorobei@mobidev.biz>
2021-07-31 20:43:28 +0300

    Added the couriers controller

---
+++ app/controllers/couriers_controller.rb
@@ -0,0 +1,7 @@
+class CouriersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'couriers/index'
+  get 'couriers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
